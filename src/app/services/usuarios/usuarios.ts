a6629166fbe5c029a1bd4b7845c0d5f4477f627f
Kesly Labio Otero <keslyotero@gmail.com>
2025-10-03 15:21:55 -0500

    Mis cambios en login

--- src/app/services/usuarios/usuarios.ts
+++ src/app/services/usuarios/usuarios.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class Usuarios {
-  
+@Injectable({ providedIn: 'root' })
+export class UsuariosService {
+  private apiUrl = 'http://localhost:3000/api/usuarios';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerUsuarios(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  obtenerUsuarioPorId(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  agregarUsuario(usuario: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, usuario);
+  }
+
+  actualizarUsuario(id: number, usuario: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, usuario);
+  }
+
+  eliminarUsuario(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }
