b21f9a216235a0c2f0816fea406deaadb4053ab9
jhoset <lopezrenteriajhosetfabian@gmail.com>
2025-10-05 18:41:38 -0500

    realice ajustes de la guia que dat de la pagina 81 hasta la ultima

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Layout } from './components/layout/layout';
+import { authGuard } from './guard/guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login').then(m => m.LoginComponent)
+    },
+    {
+        path: '',
+        component: Layout,
+        canActivate: [authGuard],
+        children: [
+            {
+                path: 'usuarios',
+                loadComponent: () => import('./pages/usuarios/usuarios').then(m => m.UsuariosComponent)
+            },
+            {
+                path: 'roles',
+                loadComponent: () => import('./pages/roles/roles').then(m => m.RolesComponent)
+            },
+            {
+                path: '',
+                redirectTo: 'usuarios',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: 'inicio',
+        loadComponent: () => import('./pages/pagina/pagina').then(m => m.InicioComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+];
